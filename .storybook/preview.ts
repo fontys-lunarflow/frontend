823f007850fb334cca8a5472adc48bcb722e59ad
Martijnkop <martijn.koppejan1@gmail.com>
2025-04-10 14:16:25 +0200

    fix themes in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,14 @@
 import type { Preview } from '@storybook/react'
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+import { lightTheme } from '../src/themes/lightTheme' 
+import { darkTheme } from '../src/themes/darkTheme' 
 
 const preview: Preview = {
   parameters: {
@@ -10,5 +20,15 @@ const preview: Preview = {
     },
   },
 };
+
+export const decorators = [withThemeFromJSXProvider({
+  themes: {
+    light: lightTheme,
+    dark: darkTheme,
+  },
+  defaultTheme: 'light',
+  Provider: ThemeProvider,
+  GlobalStyles: CssBaseline,
+})]
 
 export default preview;
