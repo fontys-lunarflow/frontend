e24eb46b536d373ca4826c8813b615dab603a743
Škrbić Veljko <494716@student.fontys.nl>
2025-06-11 23:53:39 +0200

    restore: recover original layout and page components from merge commit 40556ea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,39 @@
 import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/lib/ThemeContext";
+import SessionProvider from "@/components/SessionProvider";
+import { Roboto, Roboto_Condensed } from 'next/font/google';
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// Load Roboto font
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+// Load Roboto Condensed font
+const robotoCondensed = Roboto_Condensed({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-condensed',
+});
 
 export const metadata: Metadata = {
-  title: "LunarFlow",
-  description: "Project Management and Team Collaboration Platform",
+  title: "Material Design 3 Demo",
+  description: "A showcase of Material Design 3 components and typography",
 };
 
 export default function RootLayout({
@@ -12,9 +42,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${roboto.variable} ${robotoCondensed.variable}`}>
       <body>
-        {children}
+        {/* className={`${geistSans.variable} ${geistMono.variable} antialiased`} */}
+        <SessionProvider>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
